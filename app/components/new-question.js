c5e12cccf8236042ce28c7f0347e177775f7a95b
Caleb Stokka <caleb.stokka@gmail.com>
2016-08-19 11:27:26 -0700

    add function to allow user to ask question

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      var params = {
+        author: this.get('author'),
+        title: this.get('title'),
+        content: this.get('content'),
+      };
+      this.sendAction('save', params);
+    }
+  }
 });
--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('question', {path: '/question/:question_id'});
+  this.route('new-question');
+  this.route('add-question');
 });
 
 export default Router;
