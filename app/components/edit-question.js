09d0757d820f85e5a668b9515685ecf1b5fdc171
Caleb Stokka <caleb.stokka@gmail.com>
2016-08-19 11:36:20 -0700

    add function to allow editing questions

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(model) {
+      var params = {
+        author: this.get('author'),
+        title: this.get('title'),
+        content: this.get('content'),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', model, params);
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -3,4 +3,34 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
+  },
+
+  actions: {
+    update(model, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          model.set(key,params[key]);
+        }
+      });
+      model.save();
+    },
+    destroyQuestion(question) {
+      var answer_deletions = question.get('answers').map(function(answer) {
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function() {
+        return question.destroyRecord();
+      });
+      this.transitionTo('index');
+    },
+    save(params) {
+      var newComment = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newComment);
+      newComment.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', params.question);
+    }
   }
+});
