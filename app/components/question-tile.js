eadda1f8ca0b1652d28af00509d86d0b13fe1a12
Caleb Stokka <caleb.stokka@gmail.com>
2016-08-19 11:51:49 -0700

    add answers

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    update(model, params) {
+      this.sendAction('update', model, params);
+    },
+    delete(model) {
+      if (confirm('Would you like to remove this question?')) {
+        this.sendAction('destroyQuestion', model);
+      }
+    }
+  }
 });
--- app/models/answer.js
+++ app/models/answer.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  username: DS.attr(),
+  answerContent: DS.attr(),
+  question: DS.belongsTo('question', { async: true })
 });
