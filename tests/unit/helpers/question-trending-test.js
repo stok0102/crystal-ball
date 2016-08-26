fef715f5ef8eea2eab57144214dc3b857cff46d9
Caleb Stokka <caleb.stokka@gmail.com>
2016-08-25 17:22:35 -0700

    add helper to show glyphicon next to questions with three or more answers

---
+++ tests/unit/helpers/question-trending-test.js
@@ -0,0 +1,10 @@
+import { questionTrending } from 'crystal-ball/helpers/question-trending';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | question trending');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = questionTrending([42]);
+  assert.ok(result);
+});
